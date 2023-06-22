b9929669de7eee40947e41729075b84c53166add
Kozerema <kozeremavika@gmail.com>
2023-06-22 20:24:19 +0300

    Render users and information about them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './componets/app.component';
+import { UsersComponent } from './componets/users/users.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserDetailsComponent } from './componets/user-details/user-details.component';
+import { PostComponent } from './componets/post/post.component';
+import { PostsComponent } from './componets/posts/posts.component';
+import {UserComponent} from "./componets/user/user.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
+    PostComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
